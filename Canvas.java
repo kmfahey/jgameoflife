c6016db3f25f16ac4be0826521921a38278f93fb
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2022-11-09 19:29:41 -0800

    got automata runner loop working; added Seed and Clear buttons

--- Canvas.java
+++ Canvas.java
@@ -4,21 +4,169 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.Iterator;
 
 public class Canvas extends JComponent implements ActionListener {
-
+    final int STEP = 250;
     Color fieldColor = Color.WHITE;
+    int canvasWidth;
+    int canvasHeight;
+    int cellGridXdim;
+    int cellGridYdim;
+    int[][] cellsGrid;
+
+    boolean automataRunning = false;
+    Timer animationTimer;
+
+    public Canvas(int canvasWidthVal, int canvasHeightVal) {
+        assert canvasWidth % 10 == 0;
+        assert canvasHeight % 10 == 0;
+        canvasWidth = canvasWidthVal;
+        canvasHeight = canvasHeightVal;
+        cellGridXdim = canvasWidth / 10;
+        cellGridYdim = canvasHeight / 10;
+        cellsGrid = instanceBlankCellGrid();
+    }
+
+    private int[][] instanceBlankCellGrid() {
+        int[][] localCellsGrid = new int[cellGridXdim][cellGridYdim];
+
+        for (int xIndex = 0; xIndex < cellGridXdim; xIndex++) {
+            for (int yIndex = 0; yIndex < cellGridYdim; yIndex++) {
+                localCellsGrid[xIndex][yIndex] = 0;
+            }
+        }
+
+        return localCellsGrid;
+    }
+
+    public void clearCellGrid() {
+        cellsGrid = instanceBlankCellGrid();
+    }
 
     protected void paintComponent(Graphics graphics) {
         graphics.setColor(fieldColor);
         graphics.fillRect(0, 0, getWidth(), getHeight());
+        graphics.setColor(Color.BLACK);
+        for (int xIndex = 0; xIndex < cellGridXdim; xIndex++) {
+            for (int yIndex = 0; yIndex < cellGridYdim; yIndex++) {
+                if (cellsGrid[xIndex][yIndex] == 1) {
+                    graphics.fillRect(xIndex * 10, yIndex * 10, 10, 10);
+                }
+            }
+        }
+    }
+
+    public void seedCellGrid() {
+        ThreadLocalRandom rng = ThreadLocalRandom.current();
+        Iterator<Integer> randomInts = rng.ints((long) cellGridXdim * (long) cellGridYdim).iterator();
+        for (int xIndex = 0; xIndex < cellGridXdim; xIndex++) {
+            for (int yIndex = 0; yIndex < cellGridYdim; yIndex++) {
+                if ((int) Math.floor((double) randomInts.next() / (double) Integer.MAX_VALUE * 8) == 0) {
+                    cellsGrid[xIndex][yIndex] = 1;
+                }
+            }
+        }
+    }
+
+    public void startCellularAutomata() {
+        if (! automataRunning) {
+            System.out.println("Starting automata!");
+            automataRunning = true;
+        }
+        if (animationTimer == null) {
+            animationTimer = new Timer(STEP, this);
+            animationTimer.setActionCommand("repaint");
+            animationTimer.setRepeats(true);
+            animationTimer.start();
+        } else if (!animationTimer.isRunning()) {
+            animationTimer.restart();
+        }
+    }
+
+    public void stopCellularAutomata() {
+        if (automataRunning) {
+            automataRunning = false;
+        }
+        animationTimer.stop();
     }
 
     public void actionPerformed(ActionEvent event) {
         if (event.getActionCommand().equals("repaint")) {
-            System.out.println("Repaint event!");
+            int[][] newCellsGrid = instanceBlankCellGrid();
+
+            for (int xIndex = 0; xIndex < cellGridXdim; xIndex++) {
+                for (int yIndex = 0; yIndex < cellGridYdim; yIndex++) {
+                    int sumOfNeighbors = 0;
+                    int moddedXIndex;
+                    int moddedYIndex;
+
+                    for (int xIndexDelta = -1; xIndexDelta <= 1; xIndexDelta++) {
+                        for (int yIndexDelta = -1; yIndexDelta <= 1; yIndexDelta++) {
+                            moddedXIndex = xIndex + xIndexDelta;
+                            moddedYIndex = yIndex + yIndexDelta;
+                            if (xIndexDelta == 0 && yIndexDelta == 0
+                                    || moddedXIndex == -1 || moddedXIndex == cellGridXdim
+                                    || moddedYIndex == -1 || moddedYIndex == cellGridYdim) {
+                                continue;
+                            }
+                            sumOfNeighbors += cellsGrid[moddedXIndex][moddedYIndex];
+                        }
+                    }
+
+                    if (cellsGrid[xIndex][yIndex] == 1) {
+                        if (sumOfNeighbors < 2 || sumOfNeighbors > 3) {
+                            newCellsGrid[xIndex][yIndex] = 0;
+                        } else {
+                            newCellsGrid[xIndex][yIndex] = 1;
+                        }
+                    } else if (sumOfNeighbors == 3) {
+                        newCellsGrid[xIndex][yIndex] = 1;
+                    } else {
+                        newCellsGrid[xIndex][yIndex] = 0;
+                    }
+                }
+            }
+            cellsGrid = newCellsGrid;
             repaint();
         }
     }
 
+    private Point[] neighboringCoords(int xIndex, int yIndex) {
+        Point[] points;
+        int pointsArrayIndex = 0;
+        int moddedXIndex;
+        int moddedYIndex;
+
+        if (xIndex == 0 || xIndex == cellGridXdim - 1) {
+            if (yIndex == 0 || yIndex == cellGridYdim - 1) {
+                points = new Point[3];
+            } else {
+                points = new Point[5];
+            }
+        } else {
+            if (yIndex == 0 || yIndex == cellGridYdim - 1) {
+                points = new Point[5];
+            } else {
+                points = new Point[8];
+            }
+        }
+
+        for (int xIndexDelta = -1; xIndexDelta <= 1; xIndexDelta++) {
+            for (int yIndexDelta = -1; yIndexDelta <= 1; yIndexDelta++) {
+                moddedXIndex = xIndex + xIndexDelta;
+                moddedYIndex = yIndex + yIndexDelta;
+                if (xIndexDelta == 0 && yIndexDelta == 0
+                        || moddedXIndex == -1 || moddedXIndex == cellGridXdim
+                        || moddedYIndex == -1 || moddedYIndex == cellGridYdim) {
+                    continue;
+                }
+                points[pointsArrayIndex] = new Point(moddedXIndex, moddedYIndex);
+                pointsArrayIndex++;
+            }
+        }
+
+        return points;
+    }
 }
--- GameOfLife.java
+++ GameOfLife.java
@@ -10,16 +10,14 @@ public class GameOfLife extends JFrame {
     GridBagLayout gameLayout = new GridBagLayout();
     JPanel gamePane = new JPanel(gameLayout);
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-
-    Canvas canvas = new Canvas();
+    Canvas canvas;
 
     public GameOfLife() {
         super("Conway's Game of Life");
 
-        int screenWidth = (int) screenSize.getWidth();
-        int screenHeight = (int) screenSize.getHeight();
-        int reducedWidth = (int) Math.floor(0.8D * (double) screenWidth);
-        int reducedHeight = (int) Math.floor(0.8D * (double) screenHeight);
+        JButton startButton;
+        int reducedWidth = (int) Math.floor(0.8D * (double) screenSize.getWidth());
+        int reducedHeight = (int) Math.floor(0.8D * (double) screenSize.getHeight());
         int widthFifth = (int) ((float) reducedWidth * 0.2F);
         int heightTenth = (int) Math.floor(0.1D * (double) reducedHeight);
         int heightNineTenths = reducedHeight - heightTenth;
@@ -30,8 +28,17 @@ public class GameOfLife extends JFrame {
         gameLayout.columnWidths = new int[] { widthFifth, widthFifth, widthFifth, widthFifth, widthFifth };
         gameLayout.rowHeights = new int[] { heightNineTenths, heightTenth };
 
+        GridBagConstraints canvasConstraints = buildCanvasConstraints(0, 0, 1, 5, reducedWidth, heightNineTenths);
+        int modifiedHeight = reducedHeight - canvasConstraints.insets.top - canvasConstraints.insets.bottom;
+        int modifiedWidth = reducedWidth - canvasConstraints.insets.left - canvasConstraints.insets.right;
+        canvas = new Canvas(modifiedWidth, modifiedHeight);
         gamePane.add(canvas, buildCanvasConstraints(0, 0, 1, 5, reducedWidth, heightNineTenths));
-        gamePane.add(buildStartButton(), buildButtonConstraints(1, 4, 1, 1, widthFifth, heightTenth));
+
+        startButton = buildStartButton();
+
+        gamePane.add(buildClearButton(startButton), buildButtonConstraints(1, 2, 1, 1, widthFifth, heightTenth));
+        gamePane.add(buildSeedButton(), buildButtonConstraints(1, 3, 1, 1, widthFifth, heightTenth));
+        gamePane.add(startButton, buildButtonConstraints(1, 4, 1, 1, widthFifth, heightTenth));
 
         setContentPane(gamePane);
         setSize(reducedWidth, reducedHeight);
@@ -51,10 +58,10 @@ public class GameOfLife extends JFrame {
         GridBagConstraints gameConstraints;
         int widthExtra = containerWidth % 10;
         int heightExtra = containerHeight % 10;
-        int topInset = 30 + (int) Math.floor((double) widthExtra / 2.0);
+        int topInset = 20 + (int) Math.floor((double) widthExtra / 2.0);
         int bottomInset = 0 + (int) Math.ceil((double) widthExtra / 2.0);
-        int leftInset = 30 + (int) Math.floor((double) heightExtra / 2.0);
-        int rightInset = 30 + (int) Math.ceil((double) heightExtra / 2.0);
+        int leftInset = 20 + (int) Math.floor((double) heightExtra / 2.0);
+        int rightInset = 20 + (int) Math.ceil((double) heightExtra / 2.0);
         gameConstraints = buildConstraints(row, col, rowspan, colspan);
         gameConstraints.insets = new Insets(topInset, leftInset, bottomInset, rightInset);
         return gameConstraints;
@@ -64,12 +71,8 @@ public class GameOfLife extends JFrame {
         GridBagConstraints gameConstraints;
         int widthThird = (int) ((float) containerWidth / 3.0F);
         int heightThird = (int) ((float) containerHeight / 3.0F);
-        int topInset = heightThird;
-        int leftInset = widthThird * 2;
-        int bottomInset = (int) ((float) heightThird * 1.5F);
-        int rightInset = (int) ((float) widthThird * 0.75F);
         gameConstraints = buildConstraints(row, col, rowspan, colspan);
-        gameConstraints.insets = new Insets(topInset, leftInset, bottomInset, rightInset);
+        gameConstraints.insets = new Insets(heightThird, widthThird, heightThird, widthThird);
         return gameConstraints;
     }
 
@@ -78,7 +81,40 @@ public class GameOfLife extends JFrame {
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                button.setText(button.getText() == "Start" ? "Stop" : "Start");
+                if (button.getText() == "Start") {
+                    canvas.startCellularAutomata();
+                    button.setText("Stop");
+                } else {
+                    canvas.stopCellularAutomata();
+                    button.setText("Start");
+                }
+                canvas.repaint();
+            }
+        });
+        return button;
+    }
+
+    private JButton buildSeedButton() {
+        JButton button = new JButton("Seed");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                canvas.seedCellGrid();
+                canvas.repaint();
+            }
+        });
+        return button;
+    }
+
+    private JButton buildClearButton(JButton startButton) {
+        JButton button = new JButton("Clear");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (startButton.getText() == "Stop") {
+                    startButton.doClick();
+                }
+                canvas.clearCellGrid();
                 canvas.repaint();
             }
         });
